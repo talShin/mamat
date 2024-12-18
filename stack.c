5c4e0f3727abffa4c766414f94c0e1d186f35042
talShin <talshin23@gmail.com>
2024-12-18 16:58:16 +0200

    added stack h c file

--- stack.c
+++ stack.c
@@ -1,3 +1,23 @@
-//
-// Created by hehej on 12/18/2024.
-//
+typedef void * elem_t;
+typedef elem_t (*clone_t)(elem_t e);
+typedef void (*destroy_t)(elem_t e);
+typedef void (*print_t)(elem_t e);
+
+elem_t clone_t(elem_t e) {
+    char *original = (char *)e;
+    char *copy = malloc(strlen(original) + 1);
+    if (copy) {
+        strcpy(copy, original);
+    }
+    return (elem_t)copy;
+}
+
+void destroy_t(elem_t e) {
+    free(e);
+}
+
+void print_t(elem_t e) {
+    printf("%s\n", (char *)e);
+}
+
+
