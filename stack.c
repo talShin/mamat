c384d2b8947e43cb1101d62731d9af0d2ca9ba58
tom9798 <soustieltom@gmail.com>
2024-12-18 17:48:57 +0200

    started the stack, maybe finished

--- stack.c
+++ stack.c
@@ -1,23 +1,111 @@
-typedef void * elem_t;
-typedef elem_t (*clone_t)(elem_t e);
-typedef void (*destroy_t)(elem_t e);
-typedef void (*print_t)(elem_t e);
+#include stdlib.h
 
-elem_t clone_t(elem_t e) {
-    char *original = (char *)e;
-    char *copy = malloc(strlen(original) + 1);
-    if (copy) {
-        strcpy(copy, original);
+
+
+struct node {
+    elem_t data;
+    struct node *next;
+};
+
+struct stack {
+    struct node *top;
+    size_t size;
+    size_t capacity;
+    clone_t clone;
+    destroy_t destroy;
+    print_t print;
+};
+
+// Create a stack, need to provide the capacity of the stack and more
+struct stack* stack_create (size_t capacity) {
+    stack* s = (stack*)malloc(sizeof(stack));
+    if (NULL == s) {
+        return NULL;
     }
-    return (elem_t)copy;
+    s->top = NULL;
+    s->size = 0;
+    s->capacity = capacity;
+    return s;
 }
 
-void destroy_t(elem_t e) {
-    free(e);
+bool stack_destroy (struct stack *s) {
+    if (NULL == s) {
+        return false;
+    }
+    while (NULL != s->top) {
+        struct node *tmp = s->top;
+        s->top = s->top->next;
+        s->destroy(tmp->data);
+        free(tmp);
+    }
+    free(s);
+    return true;
 }
 
-void print_t(elem_t e) {
-    printf("%s\n", (char *)e);
+bool stack_push (struct stack *s, elem_t elem) {
+    if (NULL == s) {
+        return false;
+    }
+    if (s->size >= s->capacity) {
+        return false;
+    }
+    struct node *new_node = (struct node *)malloc(sizeof(struct node));
+    if (NULL == new_node) {
+        return false;
+    }
+    new_node->data = s->clone(elem);
+    new_node->next = s->top;
+    s->top = new_node;
+    s->size++;
+    return true;
 }
 
+void stack_pop (struct stack *s) {
+    if (NULL == s || NULL == s->top) {
+        return;
+    }
+    struct node *tmp = s->top;
+    s->top = s->top->next;
+    s->destroy(tmp->data);
+    free(tmp);
+    s->size--;
+}
 
+node* stack_peek (struct stack *s) {
+    if (NULL == s || NULL == s->top) {
+        return NULL;
+    }
+    return s->top;
+}
+
+size_t stack_size (struct stack *s) {
+    if (NULL == s) {
+        return 0;
+    }
+    return s->size;
+}
+
+bool stack_is_empty (struct stack *s) {
+    if (NULL == s) {
+        return true;
+    }
+    return 0 == s->size;
+}
+
+size_t stack_capacity (struct stack *s) {
+    if (NULL == s) {
+        return 0;
+    }
+    return s->capacity;
+}
+
+void stack_print (struct stack *s) {
+    if (NULL == s) {
+        return;
+    }
+    struct node *tmp = s->top;
+    while (NULL != tmp) {
+        s->print(tmp->data);
+        tmp = tmp->next;
+    }
+}
