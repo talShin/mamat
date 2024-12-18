f801156c30d78120fd8f9fc6c44b16536c942a25
talShin <talshin23@gmail.com>
2024-12-18 11:05:45 +0200

    started ex3

---
+++ common.h
@@ -0,0 +1,12 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <stddef.h>  /* size_t type */
+#include <stdio.h>   /* printf etc. */
+#include <stdlib.h>  /* malloc etc. */
+#include <stdbool.h> /* bool type */
+
+enum result { SUCCESS = 0, FAIL };
+typedef void * elem_t;
+
+#endif /* COMMON_H */
